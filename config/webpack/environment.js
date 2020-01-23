4cc6e80941ace9705e5178ab1a4d228efa98ffce
Richardson Dackam <richardsondx@gmail.com>
2020-01-23 14:12:52 -0500

    FIXED an issue with webpack not loading jquery

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,37 +1,12 @@
-const { environment } = require('@rails/webpacker')
-
-module.exports = environment
+const { environment } = require('@rails/webpacker');
 
 const webpack = require('webpack')
-environment.plugins.prepend('Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jquery'
-  })
-)
-
-environment.config.merge({
-  module: {
-    rules: [
-      {
-        test: require.resolve('jquery'),
-        use: [{
-          loader: 'expose-loader',
-          options: '$'
-        }, {
-          loader: 'expose-loader',
-          options: 'jQuery'
-        }]
-      },
-      {
-        test: require.resolve('rails-ujs'),
-        use: [{
-          loader: 'expose-loader',
-          options: 'Rails'
-        }]
-      }
-    ]
-  }
-});
+environment.plugins.prepend('Provide', new webpack.ProvidePlugin({
+  $: 'jquery/src/jquery',
+  jQuery: 'jquery/src/jquery',
+  jquery: 'jquery',
+  'window.jQuery': 'jquery',
+  Popper: ['popper.js', 'default']
+}))
 
 module.exports = environment;
